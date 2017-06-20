607b1d8117d4772a5577bdb15e894e2cea5aa0da
neboduus <marian.diaconu@studenti.unitn.it>
2017-06-20 12:07:22 +0200

    add webhook controller

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,28 @@
+require 'BotMessage_Dispatcher'
+
+class WebhooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def callback
+    BotMessageDispatcher.new(webhook, user).process
+    render json: nil, status: :ok
+  end
+
+  def webhook
+    params['webhook']
+  end
+
+  def from
+    webhook[:message][:from]
+  end
+
+  def user
+    @user ||= User.find_by(telegram_id: from[:id]) || register_user
+  end
+
+  def register_user
+    @user = User.find_or_initialize_by(telegram_id: from[:id])
+    @user.update_attributes!(first_name: from[:first_name], last_name: from[:last_name])
+    @user
+  end
+end
