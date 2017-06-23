3800d78823279b9cac5df6c247a3cdd77e52f30f
neboduus <marian.diaconu@studenti.unitn.it>
2017-06-23 18:01:35 +0200

    intagrate state_machine with dialog_tree

--- app/helpers/ChatScript_Manager.rb
+++ app/helpers/ChatScript_Manager.rb
@@ -22,11 +22,18 @@ class ChatScriptManager
 
   def manage
 
+    #@user.reset_user_state
+
     user_state_JSON_OOB = "[#{@user_state.to_json}]"
     v = cs_bot.volley "#{user_state_JSON_OOB}", user: user.telegram_id
 
     volley = cs_bot.volley "#{@message}", user: user.telegram_id
-    reply = volley.text
+
+    #have to figure out how to split text in adeguate group of sentences
+    reply = volley.text.to_a
+    #reply = reply.scan(/[^\.!?]+[\.!?]/).map(&:strip)
+
+
     user_state_OOB = volley.oob
 
     # Some Log Info
@@ -35,50 +42,75 @@ class ChatScriptManager
     puts "\n ### BOT_REPLY: #{reply} ### \n"
     puts "\n ### USER_STATE_RECEIVED: #{user_state_OOB} ###\n"
 
-    # Send Chatscript response to user thought telegram
-    send_message reply
+    answers = process_OOB user_state_OOB
+
+    send_message_to_telegram reply, answers
+
 
   end
 
-  def process_OOB oob
-    if oob != ''
-      state_recieved = JSON.parse(oob)
-      dot_state = state_recieved.to_dot
-      predef_responses = dot_state.predefinited_responses
+  def process_OOB user_state_OOB
+    answers=nil
+    if user_state_OOB!=''
+      old_user_state = @user_state.to_dot
+      new_user_state = eval(user_state_OOB).to_dot
 
-      if predef_responses != nil
-        #create inline keyboard
-      end
+      change_user_state_on_updates old_user_state, new_user_state
 
-      #eliminate buttons from state
-      new_state = state_recieved.except(:buttons)
-      @user.bot_command_data = new_state.json
-      save
-
-      #return inline keyboard
-
-    end
-  end
-
-  def send_message(text, options={})
-    @api.call('sendMessage', chat_id: @user.telegram_id, text: text)
-  end
-
-  def f oob
-    #checking if there are OOB messages sent by ChatScript bot
-    if(oob!="")
-      keyboard_values = oob.split(%r{,\s*})
-
-
+      keyboard_values = new_user_state.buttons.to_a
       if (keyboard_values.length>4)
         kb = keyboard_values.each_slice(2).to_a
       else
         kb = keyboard_values
       end
 
+      kb = add_emonji_on_buttons kb
 
+      answers =
+          Telegram::Bot::Types::ReplyKeyboardMarkup
+              .new(keyboard: kb, one_time_keyboard: true)
+    end
+    return answers
+  end
+
+
+  def change_user_state_on_updates old_user_state, new_user_state
+    # change user state only if state changed
+    if check_if_state_changed old_user_state, new_user_state
+      @user.set_user_state new_user_state
+    end
+  end
+
+
+  def send_message_to_telegram(text, keyboard)
+    if keyboard.nil?
+      send_distinct_messages_no_buttons text
+    else
+      send_distinct_messages text, keyboard
+    end
+  end
+
+  def send_distinct_messages(text, buttons)
+    text.each_with_index do |t, index|
+      if index == text.size - 1
+        @api.call('sendMessage', chat_id: @user.telegram_id, text: t, reply_markup: buttons)
+      else
+        @api.call('sendMessage', chat_id: @user.telegram_id, text: t)
+      end
+    end
+  end
+
+
+  def send_distinct_messages_no_buttons(text)
+    text.each_with_index do |t, index|
+      @api.call('sendMessage', chat_id: @user.telegram_id, text: t)
+    end
+  end
+
+
+  def add_emonji_on_buttons buttons
       # funzione che per qualsiasi oob mi aggiunge un emoji a mia scelta
-      kb.map! {|x|
+      buttons.map! {|x|
         case x
           when 'health', 'salute', 'healthy diet', 'healthy diet', 'dieta', 'dieta salutare', 'mangiare bene'
             "\u{1f52c}"+x
@@ -128,13 +160,22 @@ class ChatScriptManager
             x
         end
       }
+    return buttons
+  end
 
-      answers =
-          Telegram::Bot::Types::ReplyKeyboardMarkup
-              .new(keyboard: kb, one_time_keyboard: true)
-      return answers
+  #check if the state changed, return false otherwhise
+  def check_if_state_changed old_dot_state, new_dot_state
+
+    if old_dot_state.state == new_dot_state.state && old_dot_state.state == 0
+      return true if new_dot_state.health != old_dot_state.health
+      return true if new_dot_state.physical != old_dot_state.physical
+      return true if new_dot_state.coping != old_dot_state.coping
+      return true if new_dot_state.mental != old_dot_state.mental
+    else
+      return true
     end
-    return nil
+
+    return false
 
   end
 
