12eda16ef7eff0ca494a4006cc64cab4893cb3a6
neboduus <neboduus@gmail.com>
2017-11-06 15:39:13 +0100

    adds daemons gem for doing background jobs with crono

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -6,7 +6,7 @@ class Plan < ApplicationRecord
   validates :from_day, presence: { message: 'Inserisci DATA INIZIO piano.' }
   validates :to_day, presence: { message: 'Inserisci DATA FINE piano.' }
   validates :name, presence: { message: 'Il piano deve avere un nome' }, length: { maximum: 50, message: "Il nome del piano non puo' essere piu' lungo di 50 caratteri" }
-
+  validates_uniqueness_of :name, message: "tutti i piani devono avere nomi diversi. Scegli un'atro nome!"
   #validate :date_cannot_be_in_the_past, on: :create
 
   def  date_cannot_be_in_the_past
@@ -17,6 +17,14 @@ class Plan < ApplicationRecord
 
   def ita_duration
     "Dal #{self.from_day.strftime('%-m/%-d/%Y')} al #{self.to_day.strftime('%-m/%-d/%Y')} - #{(self.to_day-self.from_day).to_i} Giorni"
+  end
+
+  def has_period_exceeded?
+    self.to_day < Date.today
+  end
+
+  def has_missing_feedback?
+    Notification.joins(planning: :plan).where('notifications.done=?  AND plans.id=?', 0, self.id).count > 0
   end
 end
 
