152d04e6cc22a96bb20532b0bbd25e58e12516ec
neboduus <neboduus@gmail.com>
2017-11-06 15:40:02 +0100

    adds PlanCheckerJob in order to check when a plan finishes

--- config/cronotab.rb
+++ config/cronotab.rb
@@ -1,9 +1,9 @@
 # cronotab.rb — Crono configuration file
 
-require 'modules/cluster'
 require 'rake'
 
 Rails.app_class.load_tasks
 
 Crono.perform(ClusterJob).every 2.minutes
 Crono.perform(NotifierJob).every 2.minutes
+Crono.perform(PlanCheckerJob).every 2.minutes
