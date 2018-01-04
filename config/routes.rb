d7a7258117e4db3357a4cf47fc0d8d5ca9c7fe50
neboduus <neboduus@gmail.com>
2018-01-04 17:24:02 +0100

    changes home footer background

--- config/routes.rb
+++ config/routes.rb
@@ -85,7 +85,6 @@ Rails.application.routes.draw do
 
   # Webhooks
   post '/webhooks/telegram_vbc43edbf1614a075954dvd4bfab34l1' => 'webhooks#callback'
-  post '/webhooks/chatscript_vbc43edbf1614a075954dvd4bfab34l1/upload_health_features' => 'webhooks#upload_health_features'
 
 	# Wearable devices
 	# Actions reserved for the coach:
