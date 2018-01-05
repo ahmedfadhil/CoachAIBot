9a5c4817fb678b63e1c82a23da58c46abd82d53e
Damiano Stoffie <stoffie@gmx.com>
2018-01-05 13:36:18 +0100

    localize...

--- app/cells/objectives_cell.rb
+++ app/cells/objectives_cell.rb
@@ -47,13 +47,13 @@ class ObjectivesCell < Cell::ViewModel
 	def status
 		if model.objectives.any?
 			css_class = "list-group-item list-group-item-success"
-			content_tag :li, "Obiettivo attivato", class: css_class
+			content_tag :li, "Allenamento avviato", class: css_class
 		elsif model.objectives.any?
 			css_class = "list-group-item list-group-item-warning"
-			content_tag :li, "Obiettivo programmato", class: css_class
+			content_tag :li, "Allenamento programmato", class: css_class
 		else
 			css_class = "list-group-item list-group-item-danger"
-			content_tag :li, "Nessun obiettivo programmato", class: css_class
+			content_tag :li, "Nessun allenamento programmato", class: css_class
 		end
 	end
 
