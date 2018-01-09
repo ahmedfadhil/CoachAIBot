2b33fa33a5061be513cdbad5407d830cc1a0ff5e
neboduus <neboduus@gmail.com>
2018-01-09 09:23:15 +0100

    deletes migration change chats table column

--- db/migrate/20171030091807_change_chat_fields_name.rb
+++
@@ -1,6 +0,0 @@
-class ChangeChatFieldsName < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :chats, :coach_user_id_id, :coach_user_id
-    rename_column :chats, :user_id_id, :user_id
-  end
-end
