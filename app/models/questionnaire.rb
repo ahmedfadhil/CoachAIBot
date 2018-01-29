54c3970a9f40206def634e17fac95f283a1669e2
neboduus <neboduus@gmail.com>
2018-01-29 09:59:21 +0100

    adds uniqueness on questionnaire.title

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,4 +1,6 @@
 class Questionnaire < ApplicationRecord
   has_many :invitations, dependent: :destroy
   has_many :questionnaire_questions, dependent: :destroy
+
+  validates_uniqueness_of :title, message: "Esiste gia' un questionario con questo nome. Scegli un'atro nome!"
 end
