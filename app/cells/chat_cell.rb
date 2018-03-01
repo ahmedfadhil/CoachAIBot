de1104c16f9a6159f42956e2e3404c76a2f31be7
ahmed fadhil <fadhil@fbk.eu>
2018-03-01 11:46:32 +0100

    added locale for italian

--- app/cells/chat_cell.rb
+++ app/cells/chat_cell.rb
@@ -1,28 +1,30 @@
 class ChatCell < Cell::ViewModel
+  include ActionView::Helpers::TranslationHelper
+  
   def show
     render
   end
-
+  
   def user_status
     render
   end
-
+  
   def user_not_profiled
     render
   end
-
+  
   def user
     options[:user]
   end
-
+  
   def chats
     model
   end
-
+  
   def new_chat
     Chat.new user_id: user.id, coach_user_id: user.coach_user.id, direction: false
   end
-
+  
   def user_profile_image(user)
     if user.telegram_id.nil?
       default_image
@@ -35,9 +37,10 @@ class ChatCell < Cell::ViewModel
       end
     end
   end
-
+  
   def default_image
     'logo.png'
   end
+
 
 end
