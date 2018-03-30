28a8afa8fb032e18086b93d2866d101d2f932382
Ahmed fadhil <ahmed.fadhil@alumni.emu.edu.tr>
2018-03-30 10:19:41 +0200

    deleted options_controller

--- app/models/questionnaire_question.rb
+++ app/models/questionnaire_question.rb
@@ -1,6 +1,6 @@
 class QuestionnaireQuestion < ApplicationRecord
   belongs_to :questionnaire
   has_many :options, dependent: :destroy
-  # enum q_type: [:multiple_choice, :yes_no, :numerical] # just numbers inside the db
-  # accepts_nested_attributes_for :options, allow_destroy: true, :reject_if => lambda { |attributes| attributes[:text].blank? }
+  enum q_type: [:multiple_choice, :yes_no, :numerical] # just numbers inside the db
+  accepts_nested_attributes_for :options, allow_destroy: true, :reject_if => lambda { |attributes| attributes[:text].blank? }
 end
