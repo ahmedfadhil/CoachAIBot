170f461feaa94f6ed38a62c909da46a842fdc59b
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2018-05-02 17:56:55 +0200

    modified admin edit

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -40,3 +40,16 @@
 
 })(jQuery);
 
+var $sheet = $('.mdl-sheet');
+
+if ($sheet.length > 0) {
+    $('html').on('click', function () {
+        $sheet.removeClass('mdl-sheet--opened')
+    });
+
+    $sheet.on('click', function (event) {
+        event.stopPropagation();
+
+        $sheet.addClass('mdl-sheet--opened');
+    });
+}
